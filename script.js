25f424ad241ebb4ed7c06b87182b7c436529e2f6
dmgastrana <dmgastrana@gmail.com>
2025-09-13 00:18:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -81,16 +81,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const makeValue = document.getElementById('make').value.toLowerCase();
         const officeValue = document.getElementById('office').value.toLowerCase();
         const modalityValue = document.getElementById('modality').value.toLowerCase();
-        const servicesupportValue = document.getElementById('servicesupport').value.toLowerCase();
-
+        
 
         const filteredData = equipmentData.filter(item => {
             return (
                 (item['Serial Number'] || '').toLowerCase().includes(serialNumberValue) &&
                 (item['Make'] || '').toLowerCase().includes(makeValue) &&
                 (item['Office'] || '').toLowerCase().includes(officeValue) &&
-                (item['Modality'] || '').toLowerCase().includes(modalityValue) &&
-                (item['Service Support'].toLowerCase().includes(servicesupportValue) 
+                (item['Modality'] || '').toLowerCase().includes(modalityValue) 
+                
             );
         });
 
@@ -138,6 +137,11 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("modalOverlay").style.display = "none";
     });
 });
+
+
+
+
+
 
 
 
